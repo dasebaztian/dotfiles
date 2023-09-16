20b7c3c9218f48f041ef34dbd84ccdc972a7d1f1
Sebastián Sánchez <danielsebastiansanchezm@gmail.com>
2023-09-16 17:26:35 -0600

    Subo todo

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+killall -q polybar
+
+echo "---" | tee -a /tmp/polybar1.log
+echo "---" | tee -a /tmp/polybar2.log
+
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bar Launched..."
