432e5351957b203aec58655a54172fcc22fc5dd6
Sebastián Sánchez <danielsebastiansanchezm@gmail.com>
2023-09-20 17:16:28 -0600

    Actualizacion

---
+++ duplicateScreen
@@ -0,0 +1,1 @@
+xrandr --output eDP --output HDMI-A-0 --auto --same-as eDP 
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,6 @@ echo "---" | tee -a /tmp/polybar1.log
 echo "---" | tee -a /tmp/polybar2.log
 
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bar Launched..."
