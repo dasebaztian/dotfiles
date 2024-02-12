5fb66f0074479c9affd0c4483e042b953eaecedd
Sebastian <danielsebastiansanchezm@gmail.com>
2024-02-11 19:04:48 -0600

    Actu

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,18 +3,12 @@
 killall -q polybar
 
 echo "---" | tee -a /tmp/polybarbackground.log
-echo "---" | tee -a /tmp/polybarworkspaces.log
-echo "---" | tee -a /tmp/polybarstatus.log
-echo "---" | tee -a /tmp/polybarinfo.log
-echo "---" | tee -a /tmp/polybarweather.log
-echo "---" | tee -a /tmp/polybarnotion.log
+echo "---" | tee -a /tmp/polybarball.log
+
 
 polybar background 2>&1 | tee -a /tmp/polybarbackground.log & disown
-polybar workspaces 2>&1 | tee -a /tmp/polybarworkspaces.log & disown
-polybar status 2>&1 | tee -a /tmp/polybarstatus.log & disown
-polybar info 2>&1 | tee -a /tmp/polybarinfo.log & disown
-polybar weather 2>&1 | tee -a /tmp/polybarweather.log & disown
-polybar notion 2>&1 | tee -a /tmp/polybarnotion.log & disown
+polybar all 2>&1 | tee -a /tmp/polybarbackground.log & disown
+
 
 
 
